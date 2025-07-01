2aa6a91adeeb086bfdf1fed9a06a5378499d7d54
Prabhjot Kaur <160531659+prabhjot1309@users.noreply.github.com>
2025-07-01 11:33:53 +0530

    Add files via upload

---
+++ Age_Classifier.py
@@ -0,0 +1,21 @@
+age = int(input("Enter your age: "))
+
+if 0== age<2:
+    print("New Born Baby")
+elif 2 > age < 5:
+    print("You are a toddler.")
+elif 5 <= age < 12:
+    print("You are a kid.")
+elif age < 13:
+    print("You are a child.")
+elif 13 <= age < 20:
+    print("You are a teenager.")
+elif 20 <= age < 35:
+    print("You are young adult.")
+elif 35 <= age < 60:
+    print("You are an adult.")
+elif 60 <= age < 100: 
+    print("You are a senior citizen.")
+elif age >= 100:
+    print("You are a centenarian.")     
+
